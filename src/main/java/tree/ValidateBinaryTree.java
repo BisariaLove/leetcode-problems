de8fc9cc3a0fcaab2d44418436404d1ab01cddff
BisariaLove <bisarialove@gmail.com>
2019-02-24 00:42:17 -0800

    adding Validate Binary Tree

---
+++ src/main/java/tree/ValidateBinaryTree.java
@@ -0,0 +1,29 @@
+package tree;
+/*
+ * @author love.bisaria on 24/02/19
+ *
+ * Problem Description - https://leetcode.com/problems/validate-binary-search-tree/
+ */
+
+public class ValidateBinaryTree {
+
+    public boolean isValidBST(TreeNode root) {
+        return valid(root, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+    }
+
+    private boolean valid(TreeNode node, Double low, Double max){
+
+        if(node == null) return true;
+        return (node.val > low)
+                && (node.val < max)
+                && valid(node.left, low, new Double(node.val))
+                && valid(node.right, new Double(node.val), max);
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+}
