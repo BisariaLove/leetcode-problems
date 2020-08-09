b55edcfd2a43afd17165eb4b715b4610d39be36e
Love Bisaria <bisarialove@gmail.com>
2020-08-08 23:41:19 -0700

    adding cake thief

---
+++ src/main/java/dynamicProgramming/CakeThief.java
@@ -0,0 +1,140 @@
+package dynamicProgramming;
+
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+import static org.junit.Assert.*;
+
+
+//Write a method maxDuffelBagValue() that takes an array of cake type objects and a weight capacity, and returns the maximum monetary value the duffel bag can hold.
+
+public class CakeThief {
+    public static class CakeType {
+
+        final int weight;
+        final int value;
+
+        public CakeType(int weight, int value) {
+            this.weight = weight;
+            this.value  = value;
+        }
+    }
+
+    public static long maxDuffelBagValue(CakeType[] cakeTypes, int weightCapacity) {
+
+        long[] maxValueAtCapacity = new long[weightCapacity + 1];
+        long currentMaxValue = 0l;
+
+        for(int currentCapacity=0; currentCapacity<= weightCapacity; currentCapacity++) {
+
+            for(CakeType cakeType: cakeTypes) {
+
+                if(cakeType.weight == 0 && cakeType.value != 0) {
+
+                    throw new RuntimeException("Enccountered Infinite case");
+                }
+
+                if(cakeType.weight > currentCapacity) continue;
+
+                long valueUsingCakeType = cakeType.value + maxValueAtCapacity[currentCapacity - cakeType.weight];
+
+                currentMaxValue = Math.max(valueUsingCakeType, currentMaxValue);
+            }
+
+            maxValueAtCapacity[currentCapacity] = currentMaxValue;
+        }
+
+        return maxValueAtCapacity[maxValueAtCapacity.length - 1];
+    }
+
+
+
+
+
+
+    // tests
+
+    @Test
+    public void oneCakeTest() {
+        final CakeType[] cakeTypes = {new CakeType(2, 1)};
+        final int weightCapacity = 9;
+        final long expected = 4;
+        final long actual = maxDuffelBagValue(cakeTypes, weightCapacity);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void twoCakesTest() {
+        final CakeType[] cakeTypes = {new CakeType(4, 4), new CakeType(5, 5)};
+        final int weightCapacity = 9;
+        final long expected = 9;
+        final long actual = maxDuffelBagValue(cakeTypes, weightCapacity);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void onlyTakeLessValuableCakeTest() {
+        final CakeType[] cakeTypes = {new CakeType(4, 4), new CakeType(5, 5)};
+        final int weightCapacity = 12;
+        final long expected = 12;
+        final long actual = maxDuffelBagValue(cakeTypes, weightCapacity);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void lotsOfCakesTest() {
+        final CakeType[] cakeTypes = {
+                new CakeType(2, 3), new CakeType(3, 6), new CakeType(5, 1),
+                new CakeType(6, 1), new CakeType(7, 1), new CakeType(8, 1)
+        };
+        final int weightCapacity = 7;
+        final long expected = 12;
+        final long actual = maxDuffelBagValue(cakeTypes, weightCapacity);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void valueToWeightRatioIsNotOptimalTest() {
+        final CakeType[] cakeTypes = {new CakeType(51, 52), new CakeType(50, 50)};
+        final int weightCapacity = 100;
+        final long expected = 100;
+        final long actual = maxDuffelBagValue(cakeTypes, weightCapacity);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void zeroCapacityTest() {
+        final CakeType[] cakeTypes = {new CakeType(1, 2)};
+        final int weightCapacity = 0;
+        final long expected = 0;
+        final long actual = maxDuffelBagValue(cakeTypes, weightCapacity);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void cakeWithZeroValueAndWeightTest() {
+        final CakeType[] cakeTypes = {new CakeType(0, 0), new CakeType(2, 1)};
+        final int weightCapacity = 7;
+        final long expected = 3;
+        final long actual = maxDuffelBagValue(cakeTypes, weightCapacity);
+        assertEquals(expected, actual);
+    }
+
+    @Test(expected = Exception.class)
+    public void cakeWithNonZeroValueAndZeroWeightTest() {
+        final CakeType[] cakeTypes = {new CakeType(0, 5)};
+        final int weightCapacity = 5;
+        maxDuffelBagValue(cakeTypes, weightCapacity);
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(CakeThief.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
+    }
+}
